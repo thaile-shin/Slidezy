23d7a5054fdc5e10243ec72e8f682ce394df1b19
thaile-shin <shinichikudo9946@gmail.com>
2025-09-25 23:05:48 +0700

    Building Slidezy Project

---
+++ src/slidezy.js
@@ -0,0 +1,51 @@
+function Slidezy (selector, options = {}) {
+    this.container = document.querySelector(selector);
+    if(!this.container) {
+        console.error(`Slidezy: Container "${selector} not found"`);
+    }
+
+    this.opt = Object.assign({}, options);
+    this.slides = Array.from(this.container.children);
+
+    this._init();
+    this._createNavigation();
+}
+
+Slidezy.prototype._init = function () {
+    this.container.classList.add("slidezy-wrapper");
+
+    this._createTrack();
+};
+
+Slidezy.prototype._createTrack = function () {
+    this.track = document.createElement('div');
+    // this.track.classList.add('slidezy-track');
+    this.track.className = 'slidezy-track';
+
+    this.slides.forEach (slide => {
+        slide.classList.add('slidezy-slide');
+        this.track.appendChild(slide);
+    });
+
+    this.container.appendChild(this.track);
+};
+
+Slidezy.prototype._createNavigation = function () {
+    this.prevBtn = document.createElement("button");
+    this.nextBtn = document.createElement("button");
+
+    this.prevBtn.textContent = "<";
+    this.nextBtn.textContent = ">";
+
+    this.prevBtn.className = "slidezy-prev";
+    this.nextBtn.className = "slidezy-next";
+
+    this.container.append(this.prevBtn, this.nextBtn);
+
+    this.prevBtn.onclick = () => this.moveSlide(-1);
+    this.nextBtn.onclick = () => this.moveSlide(1);
+}
+
+Slidezy.prototype.moveSlide = function (step) {
+    console.log(step);
+}
